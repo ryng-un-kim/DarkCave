33117806c210f17f97c9814b5688fdec9e016152
Kim Ryung Un <gongjoa@gmail.com>
2018-10-25 09:35:54 +0900

    2DGP-Project
    add item.png, framework.py file,
    main.py(item IDLE action)

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from pico2d import*
 import threading
-
+import math
+import random
+import time
 
 class Map:
+    image = None
     def __init__(self):
-        self.image = load_image('map.png')
+        if Map.image == None:
+            Map.image = load_image('map.png')
 
     def draw(self):
         self.image.draw(VIEW_WIDTH/2, VIEW_HEIGHT/2)
@@ -23,6 +27,27 @@ class Wall:
 
     def draw(self):
         self.image.clip_draw(0, 0, 64, 64, self.image.x, self.image.y)
+
+
+class Items:
+    image = None
+
+    def __init__(self):
+        self.randx = [n for n in range(0, 960, 64)]
+        self.randy = [n for n in range(0, 960, 64)]
+        self.x = random.choice(self.randx)
+        self.y = random.choice(self.randy)
+        self.r = 0
+
+        if Items.image == None:
+            Items.image = load_image("Item.png")
+
+    def update(self):
+        self.y = self.y + math.sin(self.r * 5)*0.1
+        self.r = time.time()
+
+    def draw(self):
+        self.image.clip_draw(0, 0, 64, 64, self.x, self.y)
 
 
 class Weapon:
@@ -63,6 +88,7 @@ class Player:
         self.frame = 0
         self.unit = load_image('player_idle2.png')
         self.unit2 = load_image('weapon.png')
+        self.hitbox = SDL_Rect
 
     def move(self, dx=0, dy=0):
         self.x += dx
@@ -70,6 +96,12 @@ class Player:
 
     def wall(self):
         pass
+
+    def hit_by(self):
+        self.hitbox.x = self.unit.x
+        self.hitbox.y = self.unit.y
+        self.hitbox.w = 32
+        self.hitbox.h = 32
 
     def idle_update(self):
         self.frame = (self.frame + 1) % 4
@@ -114,6 +146,7 @@ class Mouse:
 def handle_events():
     global running
     global way
+    global click
     global x, y
     events = get_events()
     for event in events:
@@ -121,6 +154,14 @@ def handle_events():
             running = False
         elif event.type == SDL_MOUSEMOTION:
             mouse.move(dx=event.x, dy=VIEW_HEIGHT-1-event.y)
+        elif event.type == SDL_MOUSEBUTTONDOWN:
+            if event.button == SDL_BUTTON_LEFT:
+                print("click : ", event.x, event.y)
+                click = True
+        elif event.type == SDL_MOUSEBUTTONUP:
+            if event.button == SDL_BUTTON_LEFT:
+                print("click : ", event.x, event.y)
+                click = False
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_a:
                 player.move(dx=-1)
@@ -159,12 +200,14 @@ TILESIZE = 64
 open_canvas(VIEW_WIDTH, VIEW_HEIGHT)
 player = Player((VIEW_WIDTH/2)/TILESIZE, (VIEW_HEIGHT/2)/TILESIZE)
 way = True
+click = False
 running = True
 dirt = Map()
 wall = Wall(100, 100)
 player.idle_update()
 mouse = Mouse(100, 100)
 weapon = Weapon((VIEW_WIDTH/2)/TILESIZE, (VIEW_HEIGHT/2)/TILESIZE)
+item = Items()
 
 while running:
     main()
@@ -172,17 +215,15 @@ while running:
     player.update()
     mouse.update()
     wall.update()
+    item.update()
     clear_canvas()
     dirt.draw()
-    mouse.draw()
     player.draw()
     player.weapon_draw()
     wall.draw()
+    item.draw()
+    mouse.draw()
     update_canvas()
-
-
-
-
 
 
 close_canvas()
